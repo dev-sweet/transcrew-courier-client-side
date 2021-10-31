7300b8379d7d6804d4d51eaa1623b8029e425d0c
dev-sweet <sweetali0520@gmail.com>
2021-10-31 22:17:48 +0600

    header style change

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
           <h1 className="primary-color">Get In Touch</h1>
           <p className="contact-desc">
             Contact with us for your first delivery.And we will send you a email
-            .So please join with us we will contact with you.
+            .So please join us we will contact with you.
           </p>
         </div>
         <div className="d-flex align-items-center justify-content-center pt-5">
